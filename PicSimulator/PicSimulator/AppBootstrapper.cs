259fb788ff93053efd77d9f380554daa22028405
Jan Gerber <jan-456@t-online.de>
2016-04-04 17:01:46 +0200

    initiale commit

---
+++ PicSimulator/PicSimulator/AppBootstrapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Caliburn.Micro;
+using PicSimulator.ViewModels;
+
+namespace PicSimulator
+{
+    class AppBootstrapper : BootstrapperBase
+    {
+        public AppBootstrapper() //Einstellungen des Caliburn Framework
+        {
+            Initialize();
+        }
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<MainViewModel>();
+        }
+    }
+}
---
+++ PicSimulator/PicSimulator/ViewModels/MainViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace PicSimulator.ViewModels {
+    class MainViewModel : Screen {
+        #region fields
+        private string _testButtonContent;
+
+
+        #endregion //fields
+
+        #region properties
+        public string TestButtonContent {
+            get {
+                return _testButtonContent;
+            }
+
+            set {
+                _testButtonContent = value;
+                NotifyOfPropertyChange(() => TestButtonContent);
+            }
+        }
+        #endregion //properties
+
+        #region constructor
+        public MainViewModel() {
+            TestButtonContent = "test";
+        }
+        #endregion //constructur
+
+        #region methods
+        public void TestButton() {
+            TestButtonContent = "stringneu";
+        }
+        #endregion //methods
+
+
+    }
+}
