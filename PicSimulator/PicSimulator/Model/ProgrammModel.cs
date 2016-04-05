2b50b9a19cf35a9ea6a737f812ccaeef14aad598
Jan Gerber <jan-456@t-online.de>
2016-04-05 09:05:02 +0200

    Datei einlesen

---
+++ PicSimulator/PicSimulator/Model/ProgrammModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PicSimulator.Model {
+    class ProgrammModel {
+
+        private List<int> _opcodes;
+
+        public ProgrammModel(string filePath) {
+            int counter = 0;
+            string line;
+
+            // Read the file and display it line by line.
+            System.IO.StreamReader file = new System.IO.StreamReader(@filePath);
+            while ((line = file.ReadLine()) != null) {
+                //System.Console.WriteLine(line);
+                char[] firstChar = line.ToCharArray();
+                if(char.IsNumber(firstChar[0])) {
+                    System.Console.WriteLine(line);
+                    for (int i = 0; i < firstChar.Length; 9) {
+                        //char[] gesChar[1]
+                    }
+                }
+                counter++;
+            }
+
+            file.Close();
+            System.Console.WriteLine("There were {0} lines.", counter);
+            // Suspend the screen.
+            System.Console.ReadLine();
+        }
+
+    }
+}
--- PicSimulator/PicSimulator/ViewModels/MainViewModel.cs
+++ PicSimulator/PicSimulator/ViewModels/MainViewModel.cs
@@ -4,37 +4,76 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Caliburn.Micro;
+using System.Windows.Forms;
+using PicSimulator.Model;
 
 namespace PicSimulator.ViewModels {
-    class MainViewModel : Screen {
+    class MainViewModel : Caliburn.Micro.Screen {
         #region fields
-        private string _testButtonContent;
+        private string _openFileContent;
+        private string _fileNameContent;
 
 
         #endregion //fields
 
         #region properties
-        public string TestButtonContent {
+        public string OpenFileContent {
             get {
-                return _testButtonContent;
+                return _openFileContent;
             }
 
             set {
-                _testButtonContent = value;
-                NotifyOfPropertyChange(() => TestButtonContent);
+                _openFileContent = value;
+                NotifyOfPropertyChange(() => OpenFileContent);
             }
         }
+
+        public string FileNameContent {
+            get {
+                return _fileNameContent;
+            }
+
+            set {
+                _fileNameContent = value;
+                NotifyOfPropertyChange(() => FileNameContent);
+
+      
+            }
+        }
+
         #endregion //properties
 
         #region constructor
         public MainViewModel() {
-            TestButtonContent = "test";
+            OpenFileContent = "test";
         }
+
+
+
+
         #endregion //constructur
 
         #region methods
-        public void TestButton() {
-            TestButtonContent = "stringneu";
+        public void OpenFile() {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+
+
+            // Set filter for file extension and default file extension 
+            dlg.DefaultExt = ".png";
+            dlg.Filter = "LST Files (*.lst)|*.lst";
+
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            Nullable<bool> result = dlg.ShowDialog();
+
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true) {
+                // Open document 
+                string filename = dlg.FileName;
+                ProgrammModel programModel = new ProgrammModel(filename);
+            }
         }
         #endregion //methods
 
