78ff464aa377b2869f8052161f61179ba95cfa68
JanGerber <jan-456@t-online.de>
2016-04-22 17:39:19 +0200

    Beispiel Ausgabe eines Register

--- PicSimulator/PicSimulator/Model/Speicher.cs
+++ PicSimulator/PicSimulator/Model/Speicher.cs
@@ -45,21 +45,21 @@ namespace PicSimulator.ViewModels {
         #endregion
         public Speicher() {
             //gesamtes Register mit 0 initialisieren
-                register = new byte[256];
+                Register = new byte[256];
                 byte nullen = Convert.ToByte(0); //0000 0000
-                for (int i = 0; i < register.Length; i++) { 
-                    register[i] = nullen;
+                for (int i = 0; i < register.Length; i++) {
+                Register[i] = nullen;
                 }
             //Special Function Register initialisieren
                 byte STATUS = Convert.ToByte(24); //0001 1000
-                register[3] = STATUS;
-                register[131] = STATUS;
+                Register[3] = STATUS;
+                Register[131] = STATUS;
                 byte OPTION_REG = Convert.ToByte(255); //1111 1111
-                register[129] = OPTION_REG;
+                Register[129] = OPTION_REG;
                 byte TRISA = Convert.ToByte(31); //0001 1111
-                register[133] = TRISA;
+                Register[133] = TRISA;
                 byte TRISB = Convert.ToByte(255); //1111 1111
-                register[134] = TRISB;
+                Register[134] = TRISB;
             //IO Port init
                 IoPorts = 0;
             //W Register init
@@ -70,22 +70,22 @@ namespace PicSimulator.ViewModels {
             stack = new Stack(8);
         }
         public byte getRegister(int adresse) {
-            if (new BitArray(new byte[] { register[3] }).Get(5)) { //RP0 gesetzt / Welche Bank ist ausgewählt ?
-                return register[adresse + 128];
+            if (new BitArray(new byte[] { Register[3] }).Get(5)) { //RP0 gesetzt / Welche Bank ist ausgewählt ?
+                return Register[adresse + 128];
             } else {
-                return register[adresse];
+                return Register[adresse];
             } 
         }
         public bool getRegister(int adresse, int bitNumber)
         {
             if (new BitArray(new byte[] { register[3] }).Get(5))
             { //RP0 gesetzt / Welche Bank ist ausgewählt ?
-                System.Collections.BitArray ba = new BitArray(new byte[] { register[adresse + 128] });
+                System.Collections.BitArray ba = new BitArray(new byte[] { Register[adresse + 128] });
                 return ba.Get(bitNumber);
             }
             else
             {
-                System.Collections.BitArray ba = new BitArray(new byte[] { register[adresse] });
+                System.Collections.BitArray ba = new BitArray(new byte[] { Register[adresse] });
                 return ba.Get(bitNumber);
             }
         }
@@ -93,24 +93,24 @@ namespace PicSimulator.ViewModels {
         {
             if(adr == 2 | adr == 3 | adr == 4 | adr == 10 | adr == 11) { //Überprüfung ob das Status | PCL | FSR | PCLATH | INTCON Register gewählt wurde
                 if(wert) {
-                    register[adr + 128] = (byte)(register[adr + 128] | (1 << bitNumber));
-                    register[adr] = (byte)(register[adr] | (1 << bitNumber));
+                    Register[adr + 128] = (byte)(Register[adr + 128] | (1 << bitNumber));
+                    Register[adr] = (byte)(Register[adr] | (1 << bitNumber));
                 } else {
-                    register[adr + 128] = (byte)(register[adr + 128] & ~(1 << bitNumber));
-                    register[adr] = (byte)(register[adr] & ~(1 << bitNumber));
+                    Register[adr + 128] = (byte)(Register[adr + 128] & ~(1 << bitNumber));
+                    Register[adr] = (byte)(Register[adr] & ~(1 << bitNumber));
                 }
             } else {
-                if(new BitArray(new byte[] { register[3] }).Get(5)) { //RP0 gesetzt / Welche Bank ist ausgewählt ?
+                if(new BitArray(new byte[] { Register[3] }).Get(5)) { //RP0 gesetzt / Welche Bank ist ausgewählt ?
                     if(wert) {
-                        register[adr + 128] = (byte)(register[adr + 128] | (1 << bitNumber));
+                        Register[adr + 128] = (byte)(Register[adr + 128] | (1 << bitNumber));
                     } else {
-                        register[adr + 128] = (byte)(register[adr + 128] & ~(1 << bitNumber));
+                        Register[adr + 128] = (byte)(Register[adr + 128] & ~(1 << bitNumber));
                     }
                 } else {
                     if(wert) {
-                        register[adr] = (byte)(register[adr] | (1 << bitNumber));
+                        Register[adr] = (byte)(Register[adr] | (1 << bitNumber));
                     } else {
-                        register[adr] = (byte)(register[adr] & ~(1 << bitNumber));
+                        Register[adr] = (byte)(Register[adr] & ~(1 << bitNumber));
                     }
                 }
             }
@@ -118,28 +118,28 @@ namespace PicSimulator.ViewModels {
         }
         public void setRegister(int adr, byte wert) {
             if(adr == 2 | adr == 3 | adr == 4 | adr == 10 | adr == 11) { //Überprüfung ob das Status | PCL | FSR Register gewählt wurde
-                register[adr + 128] = wert;
-                register[adr] = wert;
+                Register[adr + 128] = wert;
+                Register[adr] = wert;
             } else {
                 if(new BitArray(new byte[] { register[3] }).Get(5)) { //RP0 gesetzt / Welche Bank ist ausgewählt ?
-                    register[adr + 128] = wert;
+                    Register[adr + 128] = wert;
                 } else {
-                    register[adr] = wert;
+                    Register[adr] = wert;
                 }
             }
         }
 
         public void addToTimer(int timeAdd) {
-            if (!((register[0x81] &  0x20)==32) ) {        //T0CS(OPTION_REG<5>) ist leer 
+            if (!((Register[0x81] &  0x20)==32) ) {        //T0CS(OPTION_REG<5>) ist leer 
                 for (int i = 0; i < timeAdd; i++) {
-                    if (register[1] == 255) {
-                        register[1]++;
+                    if (Register[1] == 255) {
+                        Register[1]++;
                         //setzte Interrupt Flag
                         if(getRegister(0x0B, 5) && getRegister(0x0B, 5)) { //Ueberpruefe ob GIE AND INTCON<5>
                             setRegister(0x0B, 2, true); //Overflow sets bit T0IF(INTCON < 2 >).
                         }
                     } else {
-                        register[1]++;
+                        Register[1]++;
                     }
                 }
             }
@@ -150,11 +150,11 @@ namespace PicSimulator.ViewModels {
         }
         public void setZeroBit(bool wert) {
             if (wert) {
-                register[3] = (byte)(register[3] | 0x04); //Zero Bit null setzten
-                register[131] = (byte)(register[3] | 0x04);
+                Register[3] = (byte)(Register[3] | 0x04); //Zero Bit null setzten
+                Register[131] = (byte)(Register[3] | 0x04);
             } else {
-                register[3] = (byte)(register[3] & 0xFB);
-                register[131] = (byte)(register[3] & 0xFB);
+                Register[3] = (byte)(Register[3] & 0xFB);
+                Register[131] = (byte)(Register[3] & 0xFB);
             }
             
         }
